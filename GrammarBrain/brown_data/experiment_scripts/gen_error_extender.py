35a096e84f9f2d553c20116afd6d52953c4da9e6
ethanp <ethanp@utexas.edu>
2013-11-30 12:50:40 -0500

    test last exper’s generalization on sentences with no punct or #s (looks ‘reasonable’)

--- GrammarBrain/brown_data/experiment_scripts/gen_error_extender.py
+++ GrammarBrain/brown_data/experiment_scripts/gen_error_extender.py
@@ -2,12 +2,13 @@ import cPickle as pickle
 import csv
 from random import shuffle
 from pybrain.datasets import SequenceClassificationDataSet
-from GrammarBrain.brown_data.util.brown_pos_map import pos_vector_mapping
+from GrammarBrain.brown_data.util.brown_pos_map import pos_vector_mapping, medium_pos_map
 from GrammarBrain.brown_data.util.get_brown_pos_sents import get_nice_sentences_as_tuples, construct_sentence_matrices
 from GrammarBrain.brown_data.util.my_seq_tester import testOnSequenceData
 
 PATH = '/Users/Ethan/Dropbox/CSyStuff/GrammarBrain/GrammarBrain/brown_data/experiment_results/Gen Error 4 to 8/'
 PICKLE = 'part_2_11:29-21:22.p'
+PICKLE_2 = 'part_4_11:30-11:21.p'
 OUT_CSV = 'extension.csv'
 
 GRAMMATICAL = (0, 1)
@@ -35,14 +36,14 @@ def insert_randomized_sequence(dataset, sentence_mat):
     for word_vector in dup_sent_mat:
         dataset.appendLinked(word_vector, UNGRAMMATICAL)
 
-def generalization_error(net, length, numb, punct):
+def generalization_error(net, length, inp_len, med, numb, punct):
     sentence_tuples = get_nice_sentences_as_tuples(MIN=length, MAX=length,
                                                    include_numbers=numb,
                                                    include_punctuation=punct)
 
-    exper_data = SequenceClassificationDataSet(inp=len(pos_vector_mapping), target=2)
+    exper_data = SequenceClassificationDataSet(inp=inp_len, target=2)
 
-    sentence_matrices = construct_sentence_matrices(sentence_tuples)
+    sentence_matrices = construct_sentence_matrices(sentence_tuples, medium=med)
     for s in sentence_matrices:
         insert_grammatical_sequence(exper_data, s)
         insert_randomized_sequence(exper_data, s)
@@ -58,7 +59,7 @@ def first_vsn():
         writer = csv.writer(output)
         for sentence_len in range(20, 30):
             print 'error on len:', sentence_len
-            gen_error = generalization_error(loaded_network, sentence_len, True, True)
+            gen_error = generalization_error(loaded_network, sentence_len, len(pos_vector_mapping), False, True, True)
             error_at_len = ['Generalization Error', sentence_len, gen_error]
             writer.writerow(error_at_len)
             print error_at_len
@@ -72,10 +73,24 @@ def second_version():
         writer = csv.writer(output)
         for sentence_len in range(20, 30):
             print 'error on len:', sentence_len
-            gen_error = generalization_error(loaded_network, sentence_len, False, False)
+            gen_error = generalization_error(loaded_network, sentence_len, len(pos_vector_mapping), False, False, False)
+            error_at_len = ['Generalization Error', sentence_len, gen_error]
+            writer.writerow(error_at_len)
+            print error_at_len
+
+def third_version():
+    print 'loading network'
+    loaded_network = pickle.load(open(PATH+PICKLE_2, 'rb'))
+    print 'loaded network'
+
+    with open(PATH+'ext_3.csv', 'wb') as output:
+        writer = csv.writer(output)
+        for sentence_len in range(5, 30):
+            print 'error on len:', sentence_len
+            gen_error = generalization_error(loaded_network, sentence_len, len(medium_pos_map), True, False, False)
             error_at_len = ['Generalization Error', sentence_len, gen_error]
             writer.writerow(error_at_len)
             print error_at_len
 
 if __name__ == '__main__':
-    second_version()
+    third_version()
